1dfe6a54510985ab52a2d3a5eabf6c6b234aab49
benjamski <benjadamski@gmail.com>
2016-09-07 22:16:00 -0400

    Fix jquery bug
    a few issues: data is being loaded async for one which was causing issues with loading
    
    jquery issue resolved mostly by wrapping in on document ready function, to avoid async issues

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -1,1 +1,13 @@
 'use strict';
+
+$(()=>{
+$('#graph-btn1').on('click', function (event) {
+  event.preventDefault();
+  $('#graph1').removeClass('hidden');
+});
+
+// TODO refactor this into one if else statement
+// do something on document ready
+
+// TODO change
+});
