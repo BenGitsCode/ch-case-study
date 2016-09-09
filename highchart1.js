98b67eb61442bb4e4a88a179d50048fcf9702747
benjamski <benjadamski@gmail.com>
2016-09-08 20:25:48 -0400

    Begin scaffolding chart function and data request
    ajax successfully requests JSON object. Just need to iterate through the object and set required data to variables for use in chart function

---
+++ highchart1.js
@@ -0,0 +1,66 @@
+'use strict';
+
+// var highChart  = {};
+// declare chart name globally both from the document ready function and our requestData function
+
+
+
+// chart: type, title text, xaxis categories, y axis title
+
+$.getJSON('http://api.population.io/1.0/population/1950/United%20States/', function(data) {
+  $.each(data, function(key,value) {
+      var series = { data: []};
+      $.each(value, function(key,val) {
+          if (key === 'name') {
+              series.name = val;
+          }
+          else
+          {
+            $.each(val, function(key,val) {
+                var d = val.split(",");
+                var x = Date.UTC(d[0],d[1],d[2]);
+                series.data.push([x,d[3]]);
+            });
+          }
+      });
+      options.series.push(series);
+  });
+
+
+
+
+
+
+
+
+// function visitorData (data) {
+//    $('#chart1').highcharts({
+//     chart: {
+//         type: 'column'
+//     },
+//     title: {
+//         text: 'Male Population by age in 1950'
+//     },
+//     xAxis: {
+//         categories: ['']
+//     },
+//     yAxis: {
+//         title: {
+//             text: 'Number of visitors'
+//         }
+//     },
+//     series: data,
+//   });
+// }
+// $(()=>{
+// // CHART CODE HERE
+//  $.ajax({
+//     url: '/URL with JSON DATA',
+//     type: 'GET',
+//     async: true,
+//     dataType: "json",
+//     success: function (data) {
+//         visitorData(data);
+//     }
+//   });
+//  });
